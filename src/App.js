dfa2f3be3da3d7e1fcec49c5ee4436ff318e9ce9
Mjay <martin_jarrin@hotmail.com>
2019-01-17 16:51:53 -0500

    working date and time

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import { FormControl, Input, Button, TextField, Select, Chip, MenuItem, Checkbox, ListItemText, InputLabel } from '@material-ui/core/';
 import { withStyles } from '@material-ui/core/styles';
 import ReactDOM from 'react-dom';
+import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
+
+//date
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+
+// import DatePicker from "react-datepicker";
+
+
+import "react-datepicker/dist/react-datepicker.css";
+
+
 import './App.css';
 
 
 
+
 const names = ['finance', 'health', 'canabbis', 'fitness', 'tech'];
+const stages = ['Ontario Place', 'Innovation Centre', 'La Maison', 'Roger Centre'];
+const countries = ['France', 'Canada', 'USA', 'England', 'China'];
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -24,7 +40,18 @@ const styles = theme => ({
   selectEmpty: {
     marginTop: theme.spacing.unit * 2,
   },
+
+
 });
+
+
+const modifiersStyles = {
+  birthday: {
+    color: 'blue',
+    backgroundColor: '#ffc107',
+  },
+
+}
 
 class App extends Component {
 
@@ -44,15 +71,25 @@ class App extends Component {
     //multiselect
 
     industryValues: [],
+    companyStage: [],
 
-    companyStage: ['innovation', 'bell theatre', 'rogers center'],
+
+    companyModel: [],
+    countries: [],
+
+    //date picker
+
+
+    startDate: new Date('2014-08-18T21:11:54'),
+    endDate: null,
+
+
 
     show: false,
 
 
     location: "",
-    startDate: null,
-    endDate: null,
+
     imageUrl: "",
     loading: false,
     amount: Number,
@@ -67,15 +104,39 @@ class App extends Component {
   }
 
   handleChange(event) {
-
-
-
     this.setState({
-
       [event.target.name]: event.target.value
 
     })
   }
+
+
+  // handleChange = ({ startDate, endDate }) => {
+  //   startDate = startDate || this.state.startDate
+  //   endDate = endDate || this.state.endDate
+  //   if (startDate.isAfter(endDate)) {
+  //     endDate = startDate
+  //   }
+
+  //   this.setState({ startDate, endDate })
+  // }
+
+  // handleStart = (startDate) => {
+  //  this.setState({
+  //    startDate : startDate
+  //  })
+  // }
+
+  // handleEnd = (endDate) => {
+  //   this.setState({
+  //     endDate : endDate
+  //   })
+  // }
+
+
+  handleDateChange = date => {
+    this.setState({ startDate: date });
+  };
 
 
   render() {
@@ -101,20 +162,16 @@ class App extends Component {
     return (
 
 
+
+
       <form className={classes.root} autoComplete="on">
-
-
-
-
-
-
 
 
         <FormControl className={classes.formControl}>
 
           <InputLabel ref={this.InputLabelRef} htmlFor="company-simple">Affiliated Company</InputLabel>
           <Select
-
+            on
             onChange={this.handleChange.bind(this)}
             value={this.state.company}
             name="company"
@@ -160,11 +217,6 @@ class App extends Component {
 
           </Select>
         </FormControl>
-
-
-
-
-
 
         <FormControl className={classes.formControl}>
           <TextField
@@ -214,7 +266,7 @@ class App extends Component {
 
 
         <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="select-multiple-checkbox" >Industry (multiselect from predefined list)</InputLabel>
+          <InputLabel htmlFor="select-multiple-checkbox" > • Industry (multiselect from predefined list)</InputLabel>
           <Select
             multiple
             name="industryValues"
@@ -234,7 +286,135 @@ class App extends Component {
         </FormControl>
 
 
+
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="select-multiple-checkbox" > • Industry (multiselect from predefined list)</InputLabel>
+          <Select
+            multiple
+            name="companyStage"
+            value={this.state.companyStage}
+            onChange={this.handleChange.bind(this)}
+            input={<Input id="select-multiple-checkbox" />}
+            renderValue={selected => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {stages.map(name => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={this.state.companyStage.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+
+
+
+
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="select-multiple-checkbox" > • Company Model(multiselect from predefined list)</InputLabel>
+          <Select
+            multiple
+            name="companyModel"
+            value={this.state.companyModel}
+            onChange={this.handleChange.bind(this)}
+            input={<Input id="select-multiple-checkbox" />}
+            renderValue={selected => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {stages.map(name => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={this.state.companyModel.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="select-multiple-checkbox" > • Countries (multiselect from predefined list)</InputLabel>
+          <Select
+            multiple
+            name="countries"
+            value={this.state.countries}
+            onChange={this.handleChange.bind(this)}
+            input={<Input id="select-multiple-checkbox" />}
+            renderValue={selected => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {countries.map(name => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={this.state.countries.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {(() => {
+          switch (this.state.opportunity) {
+            case "Events":
+              return (
+
+                <>
+                  <TextField
+                    id="filled-dense"
+                    label="Opportunity Name"
+                    // className={classes.FormControl}
+                    margin="dense"
+                    variant="filled"
+                  />
+
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container className={classes.grid} justify="space-around">
+          <DatePicker
+            margin="normal"
+            label="Date picker"
+            value={this.StartDate}
+            onChange={this.handleDateChange}
+          />
+          <TimePicker
+            margin="normal"
+            label="Time picker"
+            value={this.startDate}
+            onChange={this.handleDateChange}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
+
+</>
+            
+              )
+
+            case "Equity Financing":
+              return
+            case "ddd":
+              return
+            case "Pitch Competitions":
+              return
+            case "Innovation Challenges":
+              return
+            case "Pitch Competitions":
+              return
+            case "Grants":
+              return
+            case "Hackathons":
+              return
+            default:
+              return null;
+          }
+        })()}
+
+
+
+
+
+
       </form>
+
+
 
     );
   }
